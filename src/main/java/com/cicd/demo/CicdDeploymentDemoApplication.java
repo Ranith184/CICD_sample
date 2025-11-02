a9cecd8da8940930ddbeea1d28f1882de830a01f
ranith <ranithyadav1804@gmail.com>
2025-11-02 19:55:09 +0530

    cicd

---
+++ src/main/java/com/cicd/demo/CicdDeploymentDemoApplication.java
@@ -0,0 +1,11 @@
+package com.cicd.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdDeploymentDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CicdDeploymentDemoApplication.class, args);
+    }
+}
